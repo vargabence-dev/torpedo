44293133c893650a8796fc014776f3539865db13
vargabence-dev <vargabence.dev@gmail.com>
2022-10-26 21:02:48 +0200

    spring initializr init

---
+++ src/main/java/com/onsze/torpedo/TorpedoApplication.java
@@ -0,0 +1,13 @@
+package com.onsze.torpedo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TorpedoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TorpedoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/onsze/torpedo/TorpedoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onsze.torpedo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TorpedoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
